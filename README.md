9f2724a4a0d38999fb987e3a64d4e416f20b58f6
christian calleja <christiandcalleja@yahoo.com.ar>
2022-06-08 15:07:10 -0300

    tarea

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tarea1-backend
+Clase de servidores con node.js:
+Generar un backend con express-generator.
+Hacer una ruta con query que me devuelva un objeto persona con nombre, edad, altura, nacionalidad.
+Hacer una ruta que sume números.
+Usar método Number ejemplo ( let num1= Number(req.params.num1) esto hará que el parametro pase de formato string a number.
---
+++ tarea1.js
@@ -0,0 +1,52 @@
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Tarea 1 - backend - node y express')
+})
+
+app.listen(port, () => {
+  console.log(`App ejemplo escuchando en: http://localhost:${port}`)
+})
+
+//debe sumar dos números (uso de params).
+
+app.get('/suma/:num1/:num2', (req, res) => {
+    let num1 = Number(req.params.num1);  //si le retirars Number te concatena como strings
+    let num2 = Number(req.params.num2);
+    let resultado = num1 + num2;
+    res.json(resultado);
+    console.log(req.params, resultado);
+    console.log(num1, num2);
+    // console.log(typeof num1); //sacandole la conversión Number
+});
+
+//debe restar dos números y si uno de los números es cero decir que no se puede restar (uso de params).
+app.get('/resta/:num1/:num2', (req, res) => {
+    let num1 = Number(req.params.num1);
+    let num2 = Number(req.params.num2);
+    if (num1 == 0 || num2 == 0){
+        res.status(200).send("El número no puede ser igual a cero para hacer la resta");
+    } else {
+        let resta = num1 - num2;
+        res.json(resta);
+        console.log(req.params, resta);
+        console.log(num1, num2);
+    }
+})
+
+
+//Mostrar nombre y apellido (uso de query).
+
+app.get('/usuario', (req, res) => {
+    let nombre = req.query.nombre;
+    let apellido = req.query.apellido;
+    let edad = req.query.edad;
+    let altura = req.query.altura;
+    let nacionalidad = req.query.nacionalidad;
+    console.log(req.query);
+    res.status(200).send(` Mi nombre es:${nombre} ${apellido} , tengo ${edad} años, mido ${altura} m y soy ${nacionalidad}`);
+});
